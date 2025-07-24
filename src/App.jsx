ff3a6e10d900147b5c89e0557a9ba5313f2c207a
jared kiprop <jaredassociates@gmail.com>
2025-07-24 11:53:16 +0300

    importing the components from src toapp.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import SkillList from './components/SkillList';
+import SkillDetail from './components/SkillDetail';
+import AddSkillForm from './components/AddSkillForm';
+import Dashboard from './components/Dashboard';
 
 function App() {
-  
+    
 }
 
-export default App
+export default App;
+
