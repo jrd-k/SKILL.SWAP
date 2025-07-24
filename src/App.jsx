f7d09d2614f688a288d22d53c764002c45367bd8
jared kiprop <jaredassociates@gmail.com>
2025-07-24 11:56:09 +0300

    adding the routes

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,18 @@ import AddSkillForm from './components/AddSkillForm';
 import Dashboard from './components/Dashboard';
 
 function App() {
-    
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/skills" element={<SkillList />} />
+        <Route path="/skills/:id" element={<SkillDetail />} />
+        <Route path="/add-skill" element={<AddSkillForm />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
